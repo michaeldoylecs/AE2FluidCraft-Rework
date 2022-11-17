7a18bed394626294a240477b0a5177b279171fd9
asdflj <854865755@qq.com>
2022-11-17 09:47:50 +0800

    fluid packet decoder add slot restricted input

--- src/main/java/com/glodblock/github/client/gui/container/ContainerFluidPacketDecoder.java
+++ src/main/java/com/glodblock/github/client/gui/container/ContainerFluidPacketDecoder.java
@@ -1,15 +1,17 @@
 package com.glodblock.github.client.gui.container;
 
 import appeng.container.AEBaseContainer;
-import appeng.container.slot.SlotNormal;
 import com.glodblock.github.common.tile.TileFluidPacketDecoder;
+import com.glodblock.github.inventory.slot.FCSlotRestrictedInput;
 import net.minecraft.entity.player.InventoryPlayer;
+
+import static com.glodblock.github.loader.ItemAndBlockHolder.PACKET;
 
 public class ContainerFluidPacketDecoder extends AEBaseContainer {
 
     public ContainerFluidPacketDecoder(InventoryPlayer ipl, TileFluidPacketDecoder tile) {
         super(ipl, tile);
-        addSlotToContainer(new SlotNormal(tile.getInventory(), 0, 80, 35));
+        addSlotToContainer(new FCSlotRestrictedInput(PACKET.stack(), tile.getInventory(), 0, 80, 35, ipl));
         bindPlayerInventory(ipl, 0, 84);
     }
 
---
+++ src/main/java/com/glodblock/github/inventory/slot/FCSlotRestrictedInput.java
@@ -0,0 +1,23 @@
+package com.glodblock.github.inventory.slot;
+
+import appeng.container.slot.SlotRestrictedInput;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+
+public class FCSlotRestrictedInput extends SlotRestrictedInput {
+    private ItemStack valid;
+
+    public FCSlotRestrictedInput(ItemStack valid, IInventory i, int slotIndex, int x, int y, InventoryPlayer p) {
+        super(PlacableItemType.INSCRIBER_PLATE, i, slotIndex, x, y, p);
+        this.valid = valid;
+    }
+
+    @Override
+    public boolean isItemValid(final ItemStack i) {
+        if (i.getItem().getClass().isAssignableFrom(valid.getItem().getClass())) {
+            return true;
+        }
+        return false;
+    }
+}
