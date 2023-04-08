68c0abba950e61174a132fffae78f5c66ab28e0a
Firenoo <49818773+firenoo@users.noreply.github.com>
2023-04-08 08:22:12 -0700

    EC2 deprecation: Part 2 - Blocks

--- src/main/java/com/glodblock/github/crossmod/extracells/EC2TileEntity.java
+++ src/main/java/com/glodblock/github/crossmod/extracells/EC2TileEntity.java
@@ -1,4 +1,17 @@
 package com.glodblock.github.crossmod.extracells;
 
-public class EC2TileEntity {
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public abstract class EC2TileEntity<T extends TileEntity> extends TileEntity {
+
+    private Object oldTile;
+    private Class<? extends TileEntity> newTile;
+    public EC2TileEntity(String name, T oldTE, Class<? extends TileEntity> newTE) {
+        this.oldTile = oldTE;
+        this.newTile = newTE;
+    }
+
+    abstract TileEntity remap();
+
 }
