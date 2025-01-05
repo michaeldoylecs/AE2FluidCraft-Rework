68c0fb67972b7f7d826b0bb1987121f24a821abe
Michael Doyle <mcdoyle7227@gmail.com>
2025-01-05 15:15:15 -0500

    Move EssentiaGas check into add to view logic

--- src/main/java/com/glodblock/github/client/me/EssentiaRepo.java
+++ src/main/java/com/glodblock/github/client/me/EssentiaRepo.java
@@ -2,13 +2,10 @@ package com.glodblock.github.client.me;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidStack;
 
 import com.glodblock.github.common.item.ItemFluidDrop;
 import com.glodblock.github.crossmod.thaumcraft.AspectUtil;
@@ -18,7 +15,6 @@ import appeng.api.config.SearchBoxMode;
 import appeng.api.config.Settings;
 import appeng.api.config.SortOrder;
 import appeng.api.config.ViewItems;
-import appeng.api.storage.data.IAEFluidStack;
 import appeng.api.storage.data.IAEItemStack;
 import appeng.client.gui.widgets.IScrollSource;
 import appeng.client.gui.widgets.ISortSource;
@@ -85,20 +81,6 @@ public class EssentiaRepo extends FluidRepo {
         for (final IAEItemStack is : this.view) {
             this.dsp.add(is.getItemStack());
         }
-        Iterator<IAEItemStack> it1 = this.view.iterator();
-        while (it1.hasNext()) {
-            IAEFluidStack fluid = ItemFluidDrop.getAeFluidStack(it1.next());
-            if (!AspectUtil.isEssentiaGas(fluid)) {
-                it1.remove();
-            }
-        }
-        Iterator<ItemStack> it2 = this.dsp.iterator();
-        while (it2.hasNext()) {
-            FluidStack fluid = ItemFluidDrop.getFluidStack(it2.next());
-            if (!AspectUtil.isEssentiaGas(fluid)) {
-                it2.remove();
-            }
-        }
     }
 
     private void addEntriesToView(Iterable<IAEItemStack> entries) {
@@ -126,6 +108,10 @@ public class EssentiaRepo extends FluidRepo {
         }
 
         for (IAEItemStack is : entries) {
+            if (!AspectUtil.isEssentiaGas(ItemFluidDrop.getAeFluidStack(is))) {
+                continue;
+            }
+
             if (this.myPartitionList != null) {
                 if (!this.myPartitionList.isListed(is)) {
                     continue;
